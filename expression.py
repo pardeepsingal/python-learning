64fed0affe49d3f66bf0d8f81d5bdf087211a05b
Pardeep Kumar <pardeepsingal1@gmail.com>
2018-08-29 13:50:14 +0530

    Expression Syntax

---
+++ expression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+x=23
+y=74
+
+if(x<y):
+    print('x < y and the value of x = {}, value of y = {} '.format(x,y))
+elif(x>y):
+ print('x > y and the value of x = {}, value of y = {} '.format(x,y))
+elif(x==y):
+ print('x and y are equal and value of x = {} and value of y = {} '.format(x,y))
+else:
+ print('something else')
